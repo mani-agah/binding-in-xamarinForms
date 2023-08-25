a3ffbae594fd6133e242953dd72bcf16626049d6
mani <mani@gmail.com>
2023-08-25 23:04:07 +0330

    first commit

---
+++ App4/App4/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<MyView> GetText = new ObservableCollection<MyView>();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void BtnSave(object sender, EventArgs e)
+        {
+            MyView mv = new MyView();
+            mv.Prop = EntryText.Text;
+            this.BindingContext = new MyView();
+            ListV.ItemsSource = GetText;
+            GetText.Add(mv);
+        }
+
+        private void BtnErase(object sender, EventArgs e)
+        {
+            EntryText.Text = null;
+        }
+
+        private async void ListV_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+        }
+    }
+}
---
+++ App4/App4/MyView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App4
+{
+    internal class MyView
+    {
+        public string Prop { get; set; }
+    }
+}
